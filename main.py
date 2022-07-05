afb995cbae8425963c48c578718495406252fcc9
jcteoo <105869667+jcteoo@users.noreply.github.com>
2022-07-05 17:09:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def RESTFUL():
     if request.method == 'GET':                 # [IMPORTANT] By default, when the client first enters the URL, he is in fact sending a GET request
         #return render_template('index.html')
         return "hello world"
-
+'''
     elif request.method == 'POST':              # [IMPORTANT] A POST request will be triggered/sent to the rest API whenever a FORM (via html) is submitted
         input_text = request.form['texts']
         data = {"data": input_text}
         response = requests.post(url="http://disinformation-svc:5020/", json=data)      # [SUPER IMPORTANT] This sends the request to ANOTHER REST API to carry out the processing
         print(response.text)
         return render_template('index.html', content=response.text)       # "response.text" extracts the text data from the REST API RESPONSE
-
+'''
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8010)
